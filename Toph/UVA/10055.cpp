2cc5361a16f0fb9ecb26e7b10f23edfb2c3abffa
Md. Mahdi Hossain Hira <87439984+Mahdi-Hira53@users.noreply.github.com>
2021-10-04 14:53:28 +0600

    Add files via upload

---
+++ Toph/UVA/10055.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long int a,b;
+    while(cin>>a>>b)
+    {
+        cout<<labs(a-b)<<endl;
+    }
+
+    return 0;
+}
---
+++ Toph/UVA/10079.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    for (; cin >> n && n >= 0;)
+    {
+
+        cout << 1 + n * (n + 1) / 2 << endl;
+
+    }
+
+    return 0;
+}
